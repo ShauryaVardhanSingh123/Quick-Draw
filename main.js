a2c5b0c1ac857f834626030dd0cf70f2dc1c2477
ShauryaVardhanSingh123 <84715057+ShauryaVardhanSingh123@users.noreply.github.com>
2022-11-10 11:53:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,50 @@ sketch=quick_draw_data_set[random_number];
 console.log(sketch);
 
 document.getElementById("SketchToBeDrawn").innerHTML=sketch;
+
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  canvas.mouseReleased(classifyCanvas);
+  synth = window.speechSynthesis;
+}
+
+function draw() {
+  // Set stroke weight to 13
+  strokeWeight(13);
+  // Set stroke color to black
+  stroke(0);
+  // If mouse is pressed, draw line between previous and current mouse positions//
+  
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
+
+  function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+  }
+
+  function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+  }
+
+  function updateCanvas(){
+    background("white");
+    Math.floor(sketch);
+    }
+
+    function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(results);
+    drawn_sketch=document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+    document.getElementById("your_sketch").innerHTML="Your sketch : " + drawn_sketch
+  
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+  
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+  }
